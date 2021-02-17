78f1937a5e4f01f409e885b41b853f3506f13e99
lhr <jiro110516@gmail.com>
2021-02-17 19:42:28 +0900

    JDBCUTIL test

--- java/java_JDBC_JDBCUtilTest.java
+++ java/java_JDBC_JDBCUtilTest.java
@@ -8,6 +8,8 @@ public class JDBCUtilTest {
 	
 	public static void main(String[] args) {
 		JDBCUtil jdbc = JDBCUtil.getInstance();
+		
+		//selectList Test
 //		String sql = "select * from cart "
 //				+ " where cart_member = ?";
 //		List<Object> param = new ArrayList<>();
@@ -16,45 +18,22 @@ public class JDBCUtilTest {
 //		List<Map<String, Object>> list = jdbc.selectList(sql, param);
 //		System.out.println(list);
 		
-//		JDBCUtil jdbc = JDBCUtil.getInstance();
-//		String sql = "select prod_id from prod "
-//				+ " where prod_lgu = ?";
-//		
-//		List<Object> param = new ArrayList<>();
-//		param.add("P101");
-//		
-//		Map<String, Object> map = jdbc.selectOne(sql, param);
-//		System.out.println(map);
-		
-//		JDBCUtil jdbc = JDBCUtil.getInstance();
-//		String sql = " update tb_jdbc_board"
-//				+ " set title = ?"
-//				+ " where board_no = ?";
-//		
-//		List<Object> param = new ArrayList<>();
-//		param.add("sd");
-//		param.add("1");
-//		
-//		
-//		int result = jdbc.update(sql, param);
-//		System.out.println(result);
-		
-//		Map<String, Object> row = jdbc.selectOne(sql);
-//		if(row == null){
-//			
-//		}
-//		
-//		if(row.size() == 0){
-//			
-//		}
-		
+		//selectOne Test
 		String sql = "select * from member where mem_id = ?";
 		List<Object> param = new ArrayList<>();
 		param.add("a001");
 		
 		Map<String, Object> row = jdbc.selectOne(sql, param);
+		if(row == null){
+			
+		}
+		
+		if(row.size() == 0){
+			
+		}
 		System.out.println(row);
 		
+		//update Test
 //		String sql = "insert into tb_jdbc_board values("
 //				+ "(select nvl(max(board_no),0) + 1 from tb_jdbc_board)"
 ////				+ "testseq_seq.nextval" //시퀀스를 사용하는 경우
@@ -67,7 +46,4 @@ public class JDBCUtilTest {
 //		int result = jdbc.update(sql,param);
 //		System.out.println(result + "행이 영향을 받았습니다.");
 	}
-	
-	
-
 }
