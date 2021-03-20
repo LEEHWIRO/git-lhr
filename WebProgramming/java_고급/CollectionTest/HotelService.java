7c6fa6f5a8115f4efa8f52f6b1e8b3f20f1f7f08
lhr <jiro110516@gmail.com>
2021-03-20 15:26:28 +0900

    HotelService Java IO 기능 추가

--- WebProgramming/java_고급/CollectionTest/HotelService.java
+++ WebProgramming/java_고급/CollectionTest/HotelService.java
@@ -1,5 +1,12 @@
 package kr.or.ddit.basic;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -12,6 +19,14 @@ public class HotelService {
 	Scanner sc = new Scanner(System.in);
 	Map<String, String> checkin = new HashMap<String, String>();
 	
+	// 변수 선언
+	FileInputStream fis = null;
+	BufferedInputStream bis = null;
+	ObjectInputStream ois = null;
+	
+	FileOutputStream fos = null;
+	BufferedOutputStream bos = null;
+	ObjectOutputStream oos = null;
 	
 	public static void main(String[] args) {
 		HotelService hotelprogram = new HotelService();
@@ -56,48 +71,142 @@ public class HotelService {
 
 
 
+	@SuppressWarnings("unchecked")
 	private void roomStatus() {
-		Set<String> keySet = checkin.keySet();
 		
-		Iterator<String> it = keySet.iterator();
-		while(it.hasNext()) {
+		try {
+			/**
+			 * 바이트 단위 스트림 : 바이트 단위로 데이터를 읽거나 쓰는 스트림
+			 * FileInputStream(String name) => 파일 이름(경로 포함) name을 매개변수로 받아 입력 스트림 생성
+			 */
+			fis = new FileInputStream("d:/D_Other/hotel.txt");
 			
-			String key = it.next();
-			System.out.println("방번호 : " + key + ", 투숙객 : " + checkin.get(key));
+			/**
+			 * 보조 스트림 : 입출력 대상이 되는 파일이나 네트워크를 직접 읽거나 쓰는 기능은 없고
+			 * 말 그대로 보조 기능을 추가하는 스트림
+			 * BufferedInputStream => 바이트 단위로 읽는 스트림에 버퍼링 기능 제공
+			 */
+			bis = new BufferedInputStream(fis);
+			
+			/**
+			 * 직렬화 : 인스턴스의 어느 순간 상태를 그대로 저장하거나 네트워크를 통해 전송하는 것
+			 * 직렬화를 도와주는 자바의 보조 스트림
+			 * ObjectInputStream => InputStream을 매개변수로 받아 ObjectInputStream 생성
+			 */
+			ois = new ObjectInputStream(bis);
+			
+			// Object readObject() => ObjectInputStream 클래스 일반 메소드로 객체 데이터를 스트림으로 불러올 때 사용
+			checkin = (Map<String, String>) ois.readObject();
+			
+			Set<String> keySet = checkin.keySet();
+			
+			Iterator<String> it = keySet.iterator();
+			while(it.hasNext()) {
+				
+				String key = it.next();
+				System.out.println("방번호 : " + key + ", 투숙객 : " + checkin.get(key));
+			}
+			
+			// 작업 완료 후 스트림 닫기
+			ois.close();
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
+		
 		
 	}
 
 
 
+	@SuppressWarnings("unchecked")
 	private void checkOut() {
 		System.out.println("어느방을 체크아웃 하시겠습니까?");
 		String roomnumber = sc.nextLine();
-		
-		Set<Map.Entry<String, String>> mapSet = checkin.entrySet();
-		Iterator<Entry<String, String>> entryIt = mapSet.iterator();
-		boolean flag =false;
-		while(entryIt.hasNext()) {
-			Map.Entry<String, String> entry = entryIt.next();
+		try {
 			
-			if(entry.getKey().equals(roomnumber)) {
-				flag = true;
-				checkin.remove(roomnumber);
-				System.out.println("체크아웃 되었습니다.");
-				break;
+			/**
+			 * 바이트 단위 스트림 : 바이트 단위로 데이터를 읽거나 쓰는 스트림
+			 * FileInputStream(String name) => 파일 이름(경로 포함) name을 매개변수로 받아 입력 스트림 생성
+			 */
+			FileInputStream fis = new FileInputStream("d:/D_Other/hotel.txt");
 			
-			}else {
-				flag = false;
+			/**
+			 * 보조 스트림 : 입출력 대상이 되는 파일이나 네트워크를 직접 읽거나 쓰는 기능은 없고
+			 * 말 그대로 보조 기능을 추가하는 스트림
+			 * BufferedInputStream => 바이트 단위로 읽는 스트림에 버퍼링 기능 제공
+			 */
+			BufferedInputStream bis = new BufferedInputStream(fis);
+			
+			/**
+			 * 직렬화 : 인스턴스의 어느 순간 상태를 그대로 저장하거나 네트워크를 통해 전송하는 것
+			 * 직렬화를 도와주는 자바의 보조 스트림
+			 * ObjectInputStream => InputStream을 매개변수로 받아 ObjectInputStream 생성
+			 */
+			ObjectInputStream ois = new ObjectInputStream(bis);
+			
+			// Object readObject() => ObjectInputStream 클래스 일반 메소드로 객체 데이터를 스트림으로 불러올 때 사용
+			checkin = (Map<String, String>) ois.readObject();
+			
+			Set<Map.Entry<String, String>> mapSet = checkin.entrySet();
+			Iterator<Entry<String, String>> entryIt = mapSet.iterator();
+			boolean flag =false;
+			while(entryIt.hasNext()) {
+				Map.Entry<String, String> entry = entryIt.next();
+				
+				if(entry.getKey().equals(roomnumber)) {
+					flag = true;
+					checkin.remove(roomnumber);
+					System.out.println("체크아웃 되었습니다.");
+					
+					/**
+					 * 바이트 단위 스트림 : 바이트 단위로 데이터를 읽거나 쓰는 스트림
+					 * FileOutputStream(String name) => 파일 이름(경로 포함) name을 매개변수로 받아 출력 스트림 생성
+					 */
+					FileOutputStream fos = new FileOutputStream("d:/D_Other/hotel.txt");
+					
+					/**
+					 * 보조 스트림 : 입출력 대상이 되는 파일이나 네트워크를 직접 읽거나 쓰는 기능은 없고
+					 * 말 그대로 보조 기능을 추가하는 스트림
+					 * BufferedOutputStream => 바이트 단위로 출력하는 스트림에 버퍼링 기능 제공
+					 */
+					BufferedOutputStream bos = new BufferedOutputStream(fos);
+					
+					/**
+					 * 직렬화 : 인스턴스의 어느 순간 상태를 그대로 저장하거나 네트워크를 통해 전송하는 것
+					 * 직렬화를 도와주는 자바의 보조 스트림
+					 * ObjectOutputStream => OutputStream을 매개변수로 받아 ObjectOutputStream 생성
+					 */
+					ObjectOutputStream oos = new ObjectOutputStream(bos);
+					
+					// void writeObject(Object obj) => 클래스 일반 메소드로서 객체 데이터를 외부로 전송할 때 사용
+					oos.writeObject(checkin);
+					
+					// 작업 완료 후 스트림 닫기
+					ois.close();
+					oos.close();
+					break;
+				
+				}else {
+					flag = false;
 
+				}
+				
 			}
 			
+			if(flag==false) {
+				System.out.println(roomnumber + "번방에는 체크인한 사람이 없습니다.");
+			}else {
+				
+			}
+		}catch(IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
 		}
 		
-		if(flag==false) {
-			System.out.println(roomnumber + "번방에는 체크인한 사람이 없습니다.");
-		}else {
-			
-		}
 			
 		
 		
@@ -117,12 +226,75 @@ public class HotelService {
 		
 		if(checkin.get(roomnumber) == null) {
 			checkin.put(roomnumber, user);
+			
+			
+			try {
+			/**
+			 * 바이트 단위 스트림 : 바이트 단위로 데이터를 읽거나 쓰는 스트림
+			 * FileOutputStream(String name) => 파일 이름(경로 포함) name을 매개변수로 받아 출력 스트림 생성
+			 */
+			FileOutputStream fos = new FileOutputStream("d:/D_Other/hotel.txt");
+			
+			/**
+			 * 보조 스트림 : 입출력 대상이 되는 파일이나 네트워크를 직접 읽거나 쓰는 기능은 없고
+			 * 말 그대로 보조 기능을 추가하는 스트림
+			 * BufferedOutputStream => 바이트 단위로 출력하는 스트림에 버퍼링 기능 제공
+			 */
+			BufferedOutputStream bos = new BufferedOutputStream(fos);
+			
+			/**
+			 * 직렬화 : 인스턴스의 어느 순간 상태를 그대로 저장하거나 네트워크를 통해 전송하는 것
+			 * 직렬화를 도와주는 자바의 보조 스트림
+			 * ObjectOutputStream => OutputStream을 매개변수로 받아 ObjectOutputStream 생성
+			 */
+			ObjectOutputStream oos = new ObjectOutputStream(bos);
+			
+			// void writeObject(Object obj) => 클래스 일반 메소드로서 객체 데이터를 외부로 전송할 때 사용
+			oos.writeObject(checkin);
+			
 			System.out.println("체크인 되었습니다.");
 			
+			// 작업 완료 후 스트림 닫기
+			oos.close();
+			
+			}catch(IOException e){
+				e.printStackTrace();
+			}
 		}else if(checkin.get(roomnumber).equals(user)) {
 			checkin.put(roomnumber, user);
-			System.out.println("체크인 되었습니다.");
 			
+			try {
+				/**
+				 * 바이트 단위 스트림 : 바이트 단위로 데이터를 읽거나 쓰는 스트림
+				 * FileOutputStream(String name) => 파일 이름(경로 포함) name을 매개변수로 받아 출력 스트림 생성
+				 */
+				FileOutputStream fos = new FileOutputStream("d:/D_Other/hotel.txt");
+				
+				/**
+				 * 보조 스트림 : 입출력 대상이 되는 파일이나 네트워크를 직접 읽거나 쓰는 기능은 없고
+				 * 말 그대로 보조 기능을 추가하는 스트림
+				 * BufferedOutputStream => 바이트 단위로 출력하는 스트림에 버퍼링 기능 제공
+				 */
+				BufferedOutputStream bos = new BufferedOutputStream(fos);
+				
+				/**
+				 * 직렬화 : 인스턴스의 어느 순간 상태를 그대로 저장하거나 네트워크를 통해 전송하는 것
+				 * 직렬화를 도와주는 자바의 보조 스트림
+				 * ObjectOutputStream => OutputStream을 매개변수로 받아 ObjectOutputStream 생성
+				 */
+				ObjectOutputStream oos = new ObjectOutputStream(bos);
+				
+				// void writeObject(Object obj) => 클래스 일반 메소드로서 객체 데이터를 외부로 전송할 때 사용
+				oos.writeObject(checkin);
+				
+				System.out.println("체크인 되었습니다.");
+				
+				// 작업 완료 후 스트림 닫기
+				oos.close();
+				
+				}catch(IOException e){
+					e.printStackTrace();
+				}
 		}else {
 			System.out.println(roomnumber + "번방에는 이미 사람이 있습니다.");
 			
