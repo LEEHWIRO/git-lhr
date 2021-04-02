9afd8ad039b0d9e052413e49a77d8b57bad6bb1d
lhr <jiro110516@gmail.com>
2021-04-02 20:11:56 +0900

    JAVA RMI CLIENT

---
+++ WebProgramming/java_고급/javaRMIClient/vo/FileInfoVO.java
@@ -0,0 +1,25 @@
+package kr.or.ddit.rmi.vo;
+
+import java.io.Serializable;
+
+/**
+ * 파일 전송용 VO 
+ */
+public class FileInfoVO implements Serializable {
+	private String fileName;
+	private byte[] fileData;
+	public String getFileName() {
+		return fileName;
+	}
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+	public byte[] getFileData() {
+		return fileData;
+	}
+	public void setFileData(byte[] fileData) {
+		this.fileData = fileData;
+	}
+	
+	
+}
