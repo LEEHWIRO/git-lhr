b8768c2148ea19d35782356ad743bd0d368702a8
lhr <jiro110516@gmail.com>
2021-04-13 21:12:21 +0900

    JAVA_SEVLET

---
+++ WebProgramming/java_고급/ServletEx/src/kr/or/ddit/util/HttpServletRequestWrapperTest.java
@@ -0,0 +1,12 @@
+package kr.or.ddit.util;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+public class HttpServletRequestWrapperTest extends HttpServletRequestWrapper {
+
+	public HttpServletRequestWrapperTest(HttpServletRequest request) {
+		super(request);
+	}
+
+}
