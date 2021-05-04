69b8d9986e6f150f27f76c7c0404a08bc90baca0
lhr <jiro110516@gmail.com>
2021-05-04 17:20:08 +0900

    21

---
+++ PYTHON/HELLOWPYTON/day02/myfor01.py
@@ -0,0 +1,16 @@
+arr = [1,2,3,4,5,6,7,8,9]
+arrb = range(9)
+
+a = range(1,10)
+print(a[0])
+print(a[1])
+print(a[2])
+
+for i in arrb:
+    print("3 * " + str(i+1) + " = " + str((i+1) * 3))
+    
+for i in range(1,10):
+    print("3 * " + str(i) + " = " + str(i * 3))
+
+print("hel",end="")
+print("lo")    
