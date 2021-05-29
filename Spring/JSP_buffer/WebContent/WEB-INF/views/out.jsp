3cc64615c1c9a2aaa6df762c25d47c18b9881dbb
lhr <jiro110516@gmail.com>
2021-05-29 17:10:08 +0900

    3

---
+++ Spring/JSP_buffer/WebContent/WEB-INF/views/out.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8" buffer="2kb" autoFlush="false"%>
+<%@ page trimDirectiveWhitespaces="true" %>
+
+
+<% for(int i = 0; i< 50000; i++){ %>
+<span>안뇽</span>
+<%}%>
---
+++ Spring/JSP_buffer/src/com/jsp/servlet/BufferTestServlet.java
@@ -0,0 +1,28 @@
+package com.jsp.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/out")
+public class BufferTestServlet extends HttpServlet {
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String url="WEB-INF/views/out.jsp";
+		
+		request.getRequestDispatcher(url).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
