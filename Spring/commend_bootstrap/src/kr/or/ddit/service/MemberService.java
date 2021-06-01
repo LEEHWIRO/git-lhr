056226e3e0e1ffbd7625f314b8e72c783022254e
lhr <jiro110516@gmail.com>
2021-06-01 21:47:21 +0900

    2

--- Spring/commend_bootstrap/src/kr/or/ddit/service/MemberService.java
+++ Spring/commend_bootstrap/src/kr/or/ddit/service/MemberService.java
@@ -2,8 +2,12 @@ package kr.or.ddit.service;
 
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
 
 import kr.or.ddit.command.Criteria;
+import kr.or.ddit.command.SearchCriteria;
 import kr.or.ddit.dto.MemberVO;
 import kr.or.ddit.exception.InvalidPasswordException;
 import kr.or.ddit.exception.NotFoundIDException;
@@ -19,4 +23,6 @@ public interface MemberService {
 	// 회원리스트조회
 	List<MemberVO> getMemberList() throws SQLException;
 	List<MemberVO> getMemberList(Criteria cri) throws SQLException;
+	Map<String,Object> getMemberList(SearchCriteria cri) throws SQLException;
+	
 }
