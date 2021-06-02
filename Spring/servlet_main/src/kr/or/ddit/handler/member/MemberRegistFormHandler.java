ce164580a52124b85cda79d79c082c4741517bb9
lhr <jiro110516@gmail.com>
2021-06-02 20:20:04 +0900

    3

--- Spring/servlet_main/src/kr/or/ddit/handler/member/MemberRegistFormHandler.java
+++ Spring/servlet_main/src/kr/or/ddit/handler/member/MemberRegistFormHandler.java
@@ -1,21 +1,15 @@
 package kr.or.ddit.handler.member;
 
-import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
-import kr.or.ddit.dto.MemberVO;
 import kr.or.ddit.handler.Handler;
-import kr.or.ddit.service.MemberService;
 
-public class MemberInsertPageHandler implements Handler {
-	
+public class MemberRegistFormHandler implements Handler {
+
 	@Override
 	public String process(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		String url = "member/insertMember";
-		
+		String url = "member/regist";
 		return url;
 	}
 
